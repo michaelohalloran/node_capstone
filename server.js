6277ff60b40a4782a17e163e9dd2918f9eeee3a7
MJ <kipsang20323@gmail.com>
2018-03-15 00:20:13 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+//use bP and .json()
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Server started");
+});
